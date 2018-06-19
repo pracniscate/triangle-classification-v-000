777b8cccc4952aedbe91e356fa2fa09a820dca7d
Lorena <lorena.giddens@outlook.com>
2018-06-19 16:42:10 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,26 +7,26 @@ class Triangle
     @c = c # side 3
   end
 
-  def kind 
-    case different_triangles 
-      
+  def kind
+    case different_triangles
+
       when @a + @b + @c == 0
         raise TriangleError
       when @a < 0 || @b < 0 || @c < 0
         raise TriangleError
-      when @a + @b <= @c || @a + @c <= @b || @b + @c <= @a 
+      when @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
         raise TriangleError
-      when @a == @b && @b == @c 
-        :equilateral 
-      when @a == @b || @b == @c || @a == @c 
+      when @a == @b && @b == @c
+        :equilateral
+      when @a == @b || @b == @c || @a == @c
         :isosceles
-      when @a != @b && @b != @c && @a != @c 
-        :scalene 
-        
+      when @a != @b && @b != @c && @a != @c
+        :scalene
+
     end
   end
 
     class TriangleError < StandardError
       puts "This violates the triangle equality principle."
-    end 
+    end
 end
